d6438e83007e61a31eafba0f8a43afa7c02d03d1
Nikita Uvarov <uv.nikita@gmail.com>
2015-02-21 23:32:34 +0200

    Refactored plugin

--- marvin/daemon/local_api.py
+++ marvin/daemon/local_api.py
@@ -53,6 +53,10 @@ class MarvinDBUSService(dbus.service.Object):
     def hist(self, count):
         return [testfile for _ in range(count)]
 
+    @dbus.service.method('ua.douhack.marvin')
+    def discover(self):
+        return ['192.168.0.1', '192.168.0.2', '192.168.0.3', '192.168.0.4']
+
 
 class GLibLoopThread(threading.Thread):
     def __init__(self, stop_callback, *args, **kwargs):
--- marvin/plugin/marvin_extension.py
+++ marvin/plugin/marvin_extension.py
@@ -3,23 +3,24 @@ import dbus
 from gi.repository import Nautilus, GObject
 
 
-class ColumnExtension(GObject.GObject, Nautilus.MenuProvider):
+class MarvinExtension(GObject.GObject, Nautilus.MenuProvider):
     def __init__(self):
-        pass
+        bus = dbus.SessionBus()
+        obj = bus.get_object('ua.douhack.marvin', '/ua/douhack/marvin')
+        self.marvin = dbus.Interface(obj, 'ua.douhack.marvin')
 
     def send_file(self, menu, file, target):
         filename = file.get_name()
         os.system('notify-send "Sending {} to {}"'.format(filename, target))
 
-        bus = dbus.SessionBus()
-        obj = bus.get_object('ua.douhack.marvin', '/ua/douhack/marvin')
-        server = dbus.Interface(obj, 'ua.douhack.marvin')
-
-        server.send_file(filename, target)
+        self.marvin.send_file(filename, target)
         print('Sending file:', filename)
 
     def get_file_items(self, window, files):
-        ips = ['192.168.0.1', '192.168.0.2', '192.168.0.2']
+        if len(files) > 1:
+            return
+        file = files[0]
+        ips = self.marvin.discover()
         submenu = Nautilus.Menu()
         item = Nautilus.MenuItem(
             name='MarvinExtension::Send_files',
@@ -30,7 +31,7 @@ class ColumnExtension(GObject.GObject, Nautilus.MenuProvider):
         for i, ip in enumerate(ips):
             ip_item = Nautilus.MenuItem(name='MarvinExtension::Send{}'.format(i),
                                         label=ip)
-            ip_item.connect('activate', self.send_file, files[0], ip)
+            ip_item.connect('activate', self.send_file, file, ip)
             submenu.append_item(ip_item)
 
         return [item]
