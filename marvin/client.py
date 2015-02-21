a420e951ed1d915592408151e9129586af891523
Nikita Uvarov <uv.nikita@gmail.com>
2015-02-21 17:08:52 +0200

    Rewrite client, cast return values to python types

--- marvin/client.py
+++ marvin/client.py
@@ -4,6 +4,17 @@ import dbus
 __all__ = ['client']
 
 
-bus = dbus.SessionBus()
-obj = bus.get_object('ua.douhack.marvin', '/ua/douhack/marvin')
-client = dbus.Interface(obj, 'ua.douhack.marvin')
+class Client(object):
+    def __init__(self):
+        bus = dbus.SessionBus()
+        obj = bus.get_object('ua.douhack.marvin', '/ua/douhack/marvin')
+        self.server = dbus.Interface(obj, 'ua.douhack.marvin')
+
+    def echo(self, msg):
+        return str(self.server.echo())
+
+    def list_files(self):
+        return map(str, list(self.server.list_files()))
+
+
+client = Client()
