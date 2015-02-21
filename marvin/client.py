e0c08e917159d742b27c40c45bec561ac444edb2
Nikita Uvarov <uv.nikita@gmail.com>
2015-02-22 00:10:01 +0200

    Now client convert response to namedtuple

--- marvin/client.py
+++ marvin/client.py
@@ -6,13 +6,48 @@ __all__ = ['connect']
 
 
 def connect():
-    bus = dbus.SessionBus()
-    obj = bus.get_object('ua.douhack.marvin', '/ua/douhack/marvin')
-    client = dbus.Interface(obj, 'ua.douhack.marvin')
-    return client
+    return Client()
 
 
-def job_struct_from_tuple(data):
+def struct(func):
+    func_name = func.__name__
+
+    def inner(self, *args, **kwargs):
+        return to_struct(getattr(self.server, func_name)(*args, **kwargs))
+    return inner
+
+
+def structs(func):
+    func_name = func.__name__
+
+    def inner(self, *args, **kwargs):
+        return map(to_struct, getattr(self.server, func_name)(*args, **kwargs))
+    return inner
+
+
+class Client(object):
+    def __init__(self):
+        bus = dbus.SessionBus()
+        obj = bus.get_object('ua.douhack.marvin', '/ua/douhack/marvin')
+        self.server = dbus.Interface(obj, 'ua.douhack.marvin')
+
+    def __getattr__(self, item):
+        getattr(self.server, item)
+
+    @struct
+    def info(self):
+        pass
+
+    @structs
+    def list_sending(self):
+        pass
+
+    @structs
+    def hist(self):
+        pass
+
+
+def to_struct(data):
     return JobStuct(
         id=data[0],
         path=data[1],
