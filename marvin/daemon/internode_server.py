20bf8215b3d41b3d2d49fbf4092f248453436700
Roman Rader <roman.rader@gmail.com>
2015-02-21 21:16:06 +0200

    cat

--- marvin/daemon/internode_server.py
+++ marvin/daemon/internode_server.py
@@ -9,7 +9,7 @@ import logging
 import threading
 
 from thrift.protocol import TBinaryProtocol
-from thrift.server import TNonblockingServer, TServer
+from thrift.server import TServer
 from thrift.transport import TSocket, TTransport
 
 from Marvin import MarvinService
