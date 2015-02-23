b0d782343f01d1e0142ee205aecb0dd9ea3ebb26
Nikita Uvarov <uv.nikita@gmail.com>
2015-02-23 21:59:08 +0200

    Extracted DOWNLOAD_DIR constant

--- marvin/daemon/receive_tracking.py
+++ marvin/daemon/receive_tracking.py
@@ -6,34 +6,33 @@ import subprocess
 
 
 CHUNK_SIZE = 10 * 1024  # 10 kb
-
+DOWNLOAD_DIR = os.path.expanduser('~/Downloads/Marvin')
 
 jobs = {}
 
 
 def initiate_transfer(job_id, filename, total):
     jobs[job_id] = {
-        "job_id": job_id,
-        "filename": filename,
-        "received": 0,
-        "total": total,
+        'job_id': job_id,
+        'filename': filename,
+        'received': 0,
+        'total': total,
     }
 
-    with open(get_filename(job_id), "w"):
+    with open(get_filename(job_id), 'w'):
         pass
 
 
 def get_filename(job_id, part=True):
     job = jobs[job_id]
-    home = os.path.expanduser("~")
-    f = os.path.join(home, "Downloads", "Marvin", job["filename"])
+    f = os.path.join(DOWNLOAD_DIR, job['filename'])
     if part:
         f += ".part"
     return f
 
 
 def append_chunk(job_id, chunk):
-    with open(get_filename(job_id), "a+") as f:
+    with open(get_filename(job_id), 'a+') as f:
         f.write(chunk)
 
 
@@ -50,11 +49,11 @@ def finish_sending(job_id, expected_md5):
     if expected_md5 != received_md5:
         logging.error("WRONG MD5: expected {} but got {}".format(expected_md5, received_md5))
 
-        os.rename(get_filename(job_id, part=True), filename + ".error")
+        os.rename(get_filename(job_id, part=True), filename + '.error')
     else:
         os.rename(get_filename(job_id, part=True), filename)
         mime = mimetypes.guess_type(filename)[0]
         if mime and mime.startswith('image/'):
             subprocess.Popen(['xdg-open', filename])
         else:
-            subprocess.Popen(['xdg-open', os.path.expanduser('~/Downloads/Marvin')])
+            subprocess.Popen(['xdg-open', DOWNLOAD_DIR])
